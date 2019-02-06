3fb23f8e3c16258cc94330857a4b6f3e937287a4
Fer Hernandez <fh.1111@gmail.com>
2019-02-06 03:34:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function createMultiplier(multiplyValue) {
   return function (value) {
     return multiplyValue * value;
   };
-
+}
 
 var doubler = createMultiplier(2);
 var tripler = createMultiplier(3);
