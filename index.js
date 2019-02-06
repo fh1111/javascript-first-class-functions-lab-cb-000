1f486d1f80e3558ebf33ae15671823ebda158060
Fer Hernandez <fh.1111@gmail.com>
2019-02-06 03:36:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,10 @@ function createMultiplier(multiplyValue) {
 
 var doubler = createMultiplier(2);
 var tripler = createMultiplier(3);
+
+function multiplier(a, b) {
+  return a * b;
+}
+
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
